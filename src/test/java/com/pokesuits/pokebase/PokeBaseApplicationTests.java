07f1d567ad9fd4ff334c2eecd86e77b594729831
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-04-08 18:16:18 -0300

    estrutura inicial

---
+++ src/test/java/com/pokesuits/pokebase/PokeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokesuits.pokebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
