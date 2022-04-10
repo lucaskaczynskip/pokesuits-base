3972a7b9fb9c7ce9bb78f1754025bdbaa8113524
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-04-09 21:28:32 -0300

    correcao de nome do valor constante

--- src/main/java/com/pokesuits/pokebase/enums/TipoPokemon.java
+++ src/main/java/com/pokesuits/pokebase/enums/TipoPokemon.java
@@ -1,7 +1,7 @@
 package com.pokesuits.pokebase.enums;
 
 public enum TipoPokemon {
-    BUG(1), DARK(2), DRAGON(3), ELECTRIC(4), FAITY(5), FIGHTING(6), FIRE(7), FLYING(8), GHOST(9), GRASS(10), 
+    BUG(1), DARK(2), DRAGON(3), ELECTRIC(4), FAIRY(5), FIGHTING(6), FIRE(7), FLYING(8), GHOST(9), GRASS(10),
     GROUND(11), ICE(12), NORMAL(13), POISON(14), PSYCHIC(15), ROCK(16), STEEL(17), WATER(18);
 	
 	private TipoPokemon(int valor) {this.valor = valor;}
